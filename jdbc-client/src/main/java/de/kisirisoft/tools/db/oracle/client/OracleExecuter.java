3da4452bd96f79435287aad180e5dff9a3fd90fa
unknown <you@example.com>
2020-03-08 14:47:30 +0100

    jdbc-client

--- jdbc-client/src/main/java/de/kisirisoft/tools/db/oracle/client/OracleExecuter.java
+++ jdbc-client/src/main/java/de/kisirisoft/tools/db/oracle/client/OracleExecuter.java
@@ -10,6 +10,7 @@ public class OracleExecuter {
 	public static void main(String[] args)  {
 		Utils u = new Utils();
 		String line = "begin" ;
+		System.out.println("enter help for help");
 		while(!line.equals("exit")) {
 			line = scanner.nextLine();
 			u.executeCommandLine(line);
